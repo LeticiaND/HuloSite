a8c4826f0c3a075fda479196275b2de8ce09f449
Leticia <leticiandejesus@icloud.com>
2023-02-24 10:38:48 -0300

    salvando codigo

---
+++ index.js
@@ -0,0 +1,22 @@
+const section = document.querySelector("#quarta-secao")
+const title = document.querySelector("#title-four")
+const text = document.querySelector("#text-sec4")
+
+
+function section1() {
+    section.style.backgroundColor = "Green"
+    title.innerText = "Live Sports"
+    text.innerText = "Catch your games at home or on the go. Stream live games from major college and pro leagues including the NCAA®, NBA, NHL, NFL, and more."
+}
+
+function section2() {
+    section.style.backgroundColor = "Pink"
+    title.innerText = "Breaking News"
+    text.innerText = "Keep pace with what's going on locally and globally with trusted opinions from all the top news networks."
+}
+
+function section3() {
+    section.style.backgroundColor = "Blue"
+    title.innerText = "Biggest Events"
+    text.innerText = "Spectacular, can't-miss moments like the Olympics, Grammys®, Oscars®, Emmys®, and more."
+}
